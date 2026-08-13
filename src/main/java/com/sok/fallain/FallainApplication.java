beeecb1417070a9a1466bbf7f74dff5a35a6c843
cdkkyj123 <cdkkyj123@gmail.com>
2026-08-13 20:51:37 +0900

    first commit

---
+++ src/main/java/com/sok/fallain/FallainApplication.java
@@ -0,0 +1,13 @@
+package com.sok.fallain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FallainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FallainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sok/fallain/FallainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sok.fallain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FallainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
